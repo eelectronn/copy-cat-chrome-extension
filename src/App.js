e602cffd8a925cd83224aa5d0c1b1d4d8bb3e8d0
eelectronn <harsh.patel998@icloud.com>
2021-01-21 19:53:52 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div
+          style={{
+              backgroundColor: 'thistle',
+              height: '100vh'
+          }}
+      >
+      </div>
+    </div>
+  );
+}
+
+export default App;
