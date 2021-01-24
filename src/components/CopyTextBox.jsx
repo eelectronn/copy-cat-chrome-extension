09653ba5253da3be12c32bf018593bf6b5cd66cb
eelectronn <harsh.patel998@icloud.com>
2021-01-24 02:55:09 -0600

    added stylish button

--- src/components/CopyTextBox.jsx
+++ src/components/CopyTextBox.jsx
@@ -1,33 +1,97 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { motion } from 'framer-motion'
 import { CopyToClipboard } from 'react-copy-to-clipboard/lib/Component'
+import { Row, Col } from 'react-bootstrap'
 
 const CopyTextBox = ({ display, text }) => {
 
     return (
         <CopyToClipboard text={text}>
-            <motion.div
-                whileHover={{ scale: 1.1, transition: { duration: 0.1 } }}
-                whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
-                className='d-flex p-2 justify-content-around align-items-center'
+            <div
+                className='d-flex h-100'
                 style={{
                     backgroundColor: '#233546',
-                    minHeight: '40px',
+                    minHeight: '50px',
                     borderRadius: '10px',
-                    transition: 'transform .1s',
-                    height: '100%'
                 }}
             >
-                <div
+                <Row
+                    className='w-100 m-0'
                     style={{
-                        textAlign: 'center',
-                        color: 'white'
+                        color: 'white',
                     }}
                 >
-                    {display}
-                </div>
-            </motion.div>
+                    <Col
+                        className='d-flex align-items-center'
+                        xs={8}
+                        s={8}
+                        style={{
+                            // backgroundColor: 'pink'
+                        }}
+                    >
+                        <div
+                            className='p-2'
+                            style={{
+                                // backgroundColor: 'blue'
+                            }}
+                        >
+                            {display}
+                        </div>
+
+                    </Col>
+                    <Col
+                        className='d-flex'
+                        xs={4}
+                        s={4}
+                        style={{
+                            // backgroundColor: 'blue'
+                        }}
+                    >
+                        <Row
+                            style={{
+                                // backgroundColor: 'pink'
+                            }}
+                        >
+                            <Col
+                                xs={12}
+                                className='d-flex align-items-center'
+                                style={{
+                                    borderLeft: 'white solid 1px',
+                                    borderBottom: 'white solid 1px'
+                                }}
+                            >
+                                <div
+                                    className='p-1'
+                                    style={{
+                                        width: '100%',
+                                        textAlign: 'center'
+                                    }}
+                                >
+                                    edit
+                                </div>
+                            </Col>
+                            <Col
+                                xs={12}
+                                className='d-flex align-items-center'
+                                style={{
+                                    borderLeft: 'white solid 1px'
+                                }}
+                            >
+                                <div
+                                    className='p-1'
+                                    style={{
+
+                                        width: '100%',
+                                        textAlign: 'center'
+                                    }}
+                                >
+                                    delete
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </div>
         </CopyToClipboard>
     )
 }
