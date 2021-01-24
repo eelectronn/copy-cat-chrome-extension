180ad099985f086d3125c0468fa3b8e353dbaf4f
eelectronn <harsh.patel998@icloud.com>
2021-01-24 01:59:18 -0600

    Added copy text

--- src/components/CopyTextBox.jsx
+++ src/components/CopyTextBox.jsx
@@ -6,28 +6,29 @@ import { CopyToClipboard } from 'react-copy-to-clipboard/lib/Component'
 const CopyTextBox = ({ display, text }) => {
 
     return (
-        <motion.div
-            whileHover={{ scale: 1.1, transition: { duration: 0.1 } }}
-            whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
-            className='d-flex p-2 justify-content-center align-items-center'
-            style={{
-                backgroundColor: '#233546',
-                minHeight: '40px',
-                borderRadius: '10px',
-                transition: 'transform .1s',
-                height: '100%'
-            }}
-        >
-            <div
+        <CopyToClipboard text={text}>
+            <motion.div
+                whileHover={{ scale: 1.1, transition: { duration: 0.1 } }}
+                whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
+                className='d-flex p-2 justify-content-around align-items-center'
                 style={{
-                    textAlign: 'center',
-                    color: 'white'
+                    backgroundColor: '#233546',
+                    minHeight: '40px',
+                    borderRadius: '10px',
+                    transition: 'transform .1s',
+                    height: '100%'
                 }}
             >
-                {display}
-            </div>
-
-        </motion.div>
+                <div
+                    style={{
+                        textAlign: 'center',
+                        color: 'white'
+                    }}
+                >
+                    {display}
+                </div>
+            </motion.div>
+        </CopyToClipboard>
     )
 }
 
