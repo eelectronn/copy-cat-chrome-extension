e85c26330fbd9650063272296ac2de7addca8d73
eelectronn <harsh.patel998@icloud.com>
2021-01-27 01:21:46 -0600

    added add and delete form functionality

--- src/components/CopyTextBox.jsx
+++ src/components/CopyTextBox.jsx
@@ -1,79 +1,89 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
 import { CopyToClipboard } from 'react-copy-to-clipboard/lib/Component'
 import { Row, Col } from 'react-bootstrap'
 import Delete from '@material-ui/icons/Delete'
 import Edit from '@material-ui/icons/Edit'
 
-const CopyTextBox = ({ title, text }) => {
+import ModalForm from './ModalForm'
+
+const CopyTextBox = ({ title, text, handleUpdate }) => {
     const [hovered, setHovered] = useState(false)
     const [clicked, setClicked] = useState(false)
+    const [showModal, setShowModal] = useState(false)
+
+    const handleEdit = (updatedEntry) => {
+        handleUpdate({ title, text }, updatedEntry)
+    }
 
     return (
-        <CopyToClipboard text={text}>
-            <div
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-                onMouseDown={() => setClicked(true)}
-                onMouseUp={() => setClicked(false)}
-                className='d-flex h-100'
-                style={{
-                    backgroundColor: '#233546',
-                    minHeight: '80px',
-                    borderRadius: '10px',
-                    color: 'white',
-                    transform: clicked ? 'scale(0.9)' : hovered ? 'scale(1.1)' : '',
-                    transition: 'transform .2s'
-                }}
-            >
-                <Row
-                    className='w-100 m-0'
+        <div>
+            {showModal && <ModalForm
+                setShow={setShowModal}
+                formHeading='Edit Item'
+                values={{ title, text }}
+                saveData={handleEdit}
+            />}
+            <CopyToClipboard text={text}>
+                <div
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                    onMouseDown={() => setClicked(true)}
+                    onMouseUp={() => setClicked(false)}
+                    className='d-flex h-100'
+                    style={{
+                        backgroundColor: '#233546',
+                        minHeight: '80px',
+                        borderRadius: '10px',
+                        color: 'white',
+                        transform: clicked ? 'scale(0.9)' : hovered ? 'scale(1.1)' : '',
+                        transition: 'transform .2s'
+                    }}
                 >
-                    <Col
-                        className='d-flex align-items-center p-3'
-                        xs={10}
+                    <Row
+                        className='w-100 m-0'
                     >
-                        {title}
-
-                    </Col>
-                    {hovered && <Col
-                        className='d-flex p-0 justify-content-center'
-                        xs={2}
-                    >
-                        <Row
-                            className='w-100'
+                        <Col
+                            className='d-flex align-items-center p-3'
+                            xs={10}
                         >
-                            <Col
-                                xs={12}
-                                className='d-flex p-1 justify-content-center align-items-center'
-                                style={{
-                                    borderLeft: 'white solid 1px',
-                                    borderBottom: 'white solid 1px'
-                                }}
-                            >
-                                <Edit/>
-                            </Col>
-                            <Col
-                                xs={12}
-                                className='d-flex p-1 justify-content-center align-items-center'
-                                style={{
-                                    borderLeft: 'white solid 1px'
-                                }}
-                            >
-                                <Delete/>
-                            </Col>
-                        </Row>
-                    </Col>
-                    }
-                </Row>
-            </div>
-        </CopyToClipboard>
-    )
-}
+                            {title}
 
-CopyTextBox.propTypes = {
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+                        </Col>
+                        {hovered && <Col
+                            className='d-flex p-0 justify-content-center'
+                            xs={2}
+                        >
+                            <Row
+                                className='w-100'
+                            >
+                                <Col
+                                    xs={12}
+                                    className='d-flex p-1 justify-content-center align-items-center'
+                                    style={{
+                                        borderLeft: 'white solid 1px',
+                                        borderBottom: 'white solid 1px'
+                                    }}
+                                    onClick={() => setShowModal(true)}
+                                >
+                                    <Edit/>
+                                </Col>
+                                <Col
+                                    xs={12}
+                                    className='d-flex p-1 justify-content-center align-items-center'
+                                    style={{
+                                        borderLeft: 'white solid 1px'
+                                    }}
+                                >
+                                    <Delete/>
+                                </Col>
+                            </Row>
+                        </Col>
+                        }
+                    </Row>
+                </div>
+            </CopyToClipboard>
+        </div>
+    )
 }
 
 export default CopyTextBox
